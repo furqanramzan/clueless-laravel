6f723f1678f2024531a3cbb56813c3621e5d8c65
Muhammad Furqan <furqanramzan271996@gmail.com>
2020-03-03 13:20:56 +0500

    Commit

--- resources/views/guest/review.blade.php
+++ resources/views/guest/review.blade.php
@@ -74,13 +74,36 @@ function animate() {
   if (inView(element)) {
       element.classList.add('w-100');
   }
-//   var element = document.getElementById('puzzles_gameplay');
-//     var height = element.clientHeight;
-//   if (inView(height)) {
-//       element.classList.add('w-100');
-//   }
 }
 
+
+$(document).ready(function () {
+    $("form").submit(function (event) {
+        event.preventDefault();
+        $(':input[type="submit"]').prop('disabled', true);
+        var inputs = {};
+        $.each($('form').serializeArray(), function (i, field) {
+            inputs[field.name] = field.value;
+        });
+        var data = {
+            _token: inputs._token,
+            name: inputs.name,
+            body: inputs.body,
+        };
+
+
+        $.post("/comment/" + inputs.review, data,
+            function (data, status) {
+                if (status === 'success') {
+                    var comment = '<div class="comment-wrap"><div class="comment-block"><h4 class="mb-0">'+data.name+'</h4><p class="comment-text">' + data.body + '     </p><div class="bottom-comment"><div class="comment-date float-right">' + data.date + '</div></div></div></div>';
+                    $("#comments_list").prepend(comment);
+                    $(':input[type="submit"]').prop('disabled', false);
+                } else {
+                    alert('An error occured. Please refresh page and try again');
+                }
+            });
+    });
+});
 </script>
 @endpush
 
@@ -312,7 +335,7 @@ function animate() {
                                             <img src="{{ asset($item->image) }}" width="100px" height="50px">
                                         </div>
                                         <div class="main mostread">
-                                            <div class="cname pl-2 d-flex"
+                                            <div class="cname d-flex"
                                                 style="line-height: 19px; flex-direction: column; justify-content: flex-end;">
                                                 <span
                                                     style="font-size: 20px; margin-bottom: 5px;">{{ $item->company_name }}</span>
--- storage/app/public/js/guest.js
+++ storage/app/public/js/guest.js
@@ -14,33 +14,3 @@ $(window).scroll(function () {
         $('#header').removeClass('floatingNav');
     }
 });
-
-
-$(document).ready(function () {
-    $("form").submit(function (event) {
-        event.preventDefault();
-        $(':input[type="submit"]').prop('disabled', true);
-        var inputs = {};
-        $.each($('form').serializeArray(), function (i, field) {
-            inputs[field.name] = field.value;
-        });
-        console.log(inputs);
-        var data = {
-            _token: inputs._token,
-            name: inputs.name,
-            body: inputs.body,
-        };
-
-
-        $.post("/comment/" + inputs.review, data,
-            function (data, status) {
-                if (status === 'success') {
-                    var comment = '<div class="comment-wrap"><div class="comment-block"><h4 class="mb-0">'+data.name+'</h4><p class="comment-text">' + data.body + '     </p><div class="bottom-comment"><div class="comment-date float-right">' + data.date + '</div></div></div></div>';
-                    $("#comments_list").prepend(comment);
-                    $(':input[type="submit"]').prop('disabled', false);
-                } else {
-                    alert('An error occured. Please refresh page and try again');
-                }
-            });
-    });
-});
